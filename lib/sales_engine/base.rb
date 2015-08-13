4af0b95f40ec72969549faef87046a3a1b9de088
Josh Cheek <josh.cheek@gmail.com>
2015-08-12 21:45:06 -0600

    Small refactoring

--- lib/sales_engine/base.rb
+++ lib/sales_engine/base.rb
@@ -1,18 +1,16 @@
-SalesEngine = Class.new Class.new
+SalesEngine = Class.new(Class.new) { const_set :Base, superclass }
+
 require 'sales_engine/customer_repository'
 
-class SalesEngine
-  Base = superclass
-  class Base
-    attr_accessor :db, :customer_repository
+class SalesEngine::Base
+  attr_accessor :db, :customer_repository
 
-    def initialize(db)
-      self.db = db
-    end
+  def initialize(db)
+    self.db = db
+  end
 
-    def startup
-      self.customer_repository = CustomerRepository.new(db)
-      self
-    end
+  def startup
+    self.customer_repository = SalesEngine::CustomerRepository.new(db)
+    self
   end
 end
